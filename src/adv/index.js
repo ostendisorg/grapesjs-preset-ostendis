886addac384538442f31062eb3741127a9ac3bbb
dnuessli <daniel.nuessli@ostendis.com>
2020-11-20 18:55:42 +0100

    Added custom fonts

--- src/adv/index.js
+++ src/adv/index.js
@@ -1,7 +1,8 @@
 import grapesjs from 'grapesjs';
+import styles from './styles';
 
-export default grapesjs.plugins.add('gjs-preset-ostendis-adv', (editor, opts) => {
-  let c = opts || {};
+export default grapesjs.plugins.add('gjs-preset-ostendis-adv', (editor, opts = {}) => {
+  let c = opts;
   let config = editor.getConfig();
   let pfx = config.stylePrefix;
 
@@ -65,134 +66,6 @@ export default grapesjs.plugins.add('gjs-preset-ostendis-adv', (editor, opts) =>
     buttonApplyBlkText: 'Apply here',
     textBlkLabel: 'Text',
     assetsModalTitle: c.assetsModalTitle || 'Select image',
-    styleManagerSectors: [{
-        name: 'Dimension',
-        open: false,
-        buildProps: ['width', 'height', 'max-width', 'min-height', 'margin', 'padding'],
-        properties:[{
-          property: 'margin',
-          properties:[
-            { name: 'Top', property: 'margin-top'},
-            { name: 'Left', property: 'margin-left'},
-            { name: 'Right', property: 'margin-right'},
-            { name: 'Bottom', property: 'margin-bottom'}
-          ],
-        },{
-          property  : 'padding',
-          properties:[
-            { name: 'Top', property: 'padding-top'},
-            { name: 'Right', property: 'padding-right'},
-            { name: 'Bottom', property: 'padding-bottom'},
-            { name: 'Left', property: 'padding-left'}
-          ],
-        }],
-      },{
-        name: 'Typography',
-        open: false,
-        buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align', 'text-decoration', 'font-style', 'vertical-align', 'text-shadow'],
-        properties:[
-          { name: 'Font', property: 'font-family'},
-          { name: 'Weight', property: 'font-weight'},
-          { name: 'Font color', property: 'color'},
-          {
-            property: 'text-align',
-            type: 'radio',
-            defaults: 'left',
-            list: [
-              { value: 'left', name: 'Left', className: 'fa fa-align-left'},
-              { value: 'center', name: 'Center', className: 'fa fa-align-center' },
-              { value: 'right', name: 'Right', className: 'fa fa-align-right'},
-              { value: 'justify', name: 'Justify', className: 'fa fa-align-justify'}
-            ],
-          },{
-            property: 'text-decoration',
-            type: 'radio',
-            defaults: 'none',
-            list: [
-              { value: 'none', name: 'None', className: 'fa fa-times'},
-              { value: 'underline', name: 'underline', className: 'fa fa-underline' },
-              { value: 'line-through', name: 'Line-through', className: 'fa fa-strikethrough'}
-            ],
-          },{
-            property: 'font-style',
-            type: 'radio',
-            defaults: 'normal',
-            list: [
-              { value: 'normal', name: 'Normal', className: 'fa fa-font'},
-              { value: 'italic', name: 'Italic', className: 'fa fa-italic'}
-            ],
-          },{
-            property: 'vertical-align',
-            type: 'select',
-            defaults: 'baseline',
-            list: [
-              { value: 'baseline'},
-              { value: 'top'},
-              { value: 'middle'},
-              { value: 'bottom'}
-            ],
-          },{
-            property: 'text-shadow',
-            properties: [
-              { name: 'X position', property: 'text-shadow-h'},
-              { name: 'Y position', property: 'text-shadow-v'},
-              { name: 'Blur', property: 'text-shadow-blur'},
-              { name: 'Color', property: 'text-shadow-color'}
-            ],
-        }],
-      },{
-        name: 'Decorations',
-        open: false,
-        buildProps: ['background-color', 'border-collapse', 'border-radius', 'border', 'background'],
-        properties: [{
-          property: 'background-color',
-          name: 'Background',
-        },{
-          property: 'border-radius',
-          properties  : [
-            { name: 'Top', property: 'border-top-left-radius'},
-            { name: 'Right', property: 'border-top-right-radius'},
-            { name: 'Bottom', property: 'border-bottom-left-radius'},
-            { name: 'Left', property: 'border-bottom-right-radius'}
-          ],
-        },{
-          property: 'border-collapse',
-          type: 'radio',
-          defaults: 'separate',
-          list: [
-            { value: 'separate', name: 'No'},
-            { value: 'collapse', name: 'Yes'}
-          ],
-        },
-        /*
-        { // Too much low support
-          property: 'box-shadow',
-          properties: [
-            { name: 'X position', property: 'box-shadow-h'},
-            { name: 'Y position', property: 'box-shadow-v'},
-            { name: 'Blur', property: 'box-shadow-blur'},
-            { name: 'Spread', property: 'box-shadow-spread'},
-            { name: 'Color', property: 'box-shadow-color'},
-            { name: 'Shadow type', property: 'box-shadow-type'}
-          ],
-        },*/{
-          property: 'border',
-          properties: [
-            { name: 'Width', property: 'border-width', defaults: '0'},
-            { name: 'Style', property: 'border-style'},
-            { name: 'Color', property: 'border-color'},
-          ],
-        },{
-          property: 'background',
-          properties: [
-            { name: 'Image', property: 'background-image'},
-            { name: 'Repeat', property:   'background-repeat'},
-            { name: 'Position', property: 'background-position'},
-            { name: 'Attachment', property: 'background-attachment'},
-            { name: 'Size', property: 'background-size'}
-          ],
-        }],
-      }]
   };
 
   // Change some config
@@ -215,10 +88,6 @@ export default grapesjs.plugins.add('gjs-preset-ostendis-adv', (editor, opts) =>
   // Add buttons
   let importButtons = require('./buttons');
   importButtons(c);
-
-  // Load style manager
-  //let importStyle = require('../style');
-  //importStyle(c);
 
   // Set default template if the canvas is empty
   if(!editor.getHtml() && c.defaultTemplate){
@@ -288,17 +157,25 @@ export default grapesjs.plugins.add('gjs-preset-ostendis-adv', (editor, opts) =>
     openBlocksBtn && openBlocksBtn.set('active', 1);
 
 
-     // Beautify tooltips
-     var titles = document.querySelectorAll('*[data-tooltip-pos]')
+    // Beautify tooltips
+    var titles = document.querySelectorAll('*[data-tooltip-pos]')
 
-     for (var i = 0; i < titles.length; i++) {
+    for (var i = 0; i < titles.length; i++) {
        var el = titles[i]
        var title = el.getAttribute('title')
        title = title ? title.trim() : ''
        if (!title) break
        el.setAttribute('data-tooltip', title)
        el.setAttribute('title', '')
-     }
-     
+    }
+
+    // Load styles (manager)
+    styles(editor);
+
+    //Add clear function
+    const sm = editor.StyleManager;
+    sm.getConfig().clearProperties = 1;
+    sm.render();
+
   });
 });
---
+++ src/adv/styles.js
@@ -0,0 +1,47 @@
+export default (editor) => {
+  const sm = editor.StyleManager;
+
+  const removed = sm.removeSector('typography');
+
+  if(removed) {
+    var sector = sm.addSector('typography',{
+      name: 'Typography',
+      open: false,
+      buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align', 'text-shadow'],
+      properties: [{
+        property: 'text-align',
+        list: [
+            { value: 'left', className: 'fa fa-align-left' },
+            { value: 'center', className: 'fa fa-align-center'  },
+            { value: 'right', className: 'fa fa-align-right' },
+            { value: 'justify', className: 'fa fa-align-justify' },
+        ],
+      },{
+        property: 'font-family',
+        name: 'Font',
+        list: [
+          { name: 'Calibri', value: 'Calibri, sans-serif' },
+          { name: 'Calibri Light', value: 'Calibri Light, sans-serif' },
+          { name: 'Open Sans', value: 'Open Sans, sans-serif' },
+          { name: 'Open Sans Condensed', value: 'Open Sans Condensed, sans-serif' },
+          { name: 'Roboto', value: 'Roboto, sans-serif' },
+          { name: 'Roboto Slab', value: 'Roboto Slab, serif' },
+          { name: 'Merriweather', value: 'Merriweather, serif' },
+          { name: 'PT Serif', value: 'PT Serif, serif' },
+          { name: 'Arial', value: 'Arial, Helvetica, sans-serif' },
+          { name: 'Helvetica', value: 'Helvetica, Arial, sans-serif' },
+          { name: 'Comic Sans MS', value: 'Comic Sans MS, cursive' },
+          { name: 'Courier New', value: 'Courier New, Courier, monospace' },
+          { name: 'Georgia', value: 'Georgia, serif' },
+          { name: 'Lucida Sans Unicode', value: 'Lucida Sans Unicode, Lucida Grande, sans-serif' },
+          { name: 'Tahoma', value: 'Tahoma, Geneva, sans-serif' },
+          { name: 'Times New Roman', value: 'imes New Roman, Times, serif' },
+          { name: 'Trebuchet MS', value: 'Trebuchet MS, Helvetica, sans-serif' },
+          { name: 'Verdana', value: 'Verdana, Geneva, sans-serif' },
+          { name: 'Brush Script MT', value: 'Brush Script MT, sans-serif' },
+          { name: 'Impact', value: 'Impact, Charcoal, sans-serif' }
+        ]
+      }]
+    }, { at: 0 });
+  }
+}
