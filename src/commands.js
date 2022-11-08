1a45038263bc90d45e5744e7d9f31ae15b471510
Daniel Nüssli <daniel.nuessli@ostendis.com>
2022-11-08 14:24:38 +0100

    Bugfix command buttons

--- src/commands.js
+++ src/commands.js
@@ -1,41 +1,40 @@
-define(function() {
+define(function () {
   return (opt = {}) => {
     let editor = opt.editor;
     let cmdm = editor.Commands;
 
     // Overwrite export template after the editor is loaded
     // (default commands are loaded after plugins)
-    cmdm.add('undo', {
+    cmdm.add("undo", {
       run(editor, sender) {
-        sender.set('active', 0);
+        sender.set("active", 0);
         editor.UndoManager.undo(1);
-      }
+      },
     });
-    
-    cmdm.add('redo', {
+
+    cmdm.add("redo", {
       run(editor, sender) {
-        sender.set('active', 0);
+        sender.set("active", 0);
         editor.UndoManager.redo(1);
-      }
+      },
     });
 
-    cmdm.add('set-device-desktop', {
+    cmdm.add("set-device-desktop", {
       run(editor) {
-        editor.setDevice('Desktop');
-      }
+        editor.setDevice("Desktop");
+      },
     });
 
-    cmdm.add('set-device-tablet', {
+    cmdm.add("set-device-tablet", {
       run(editor) {
-        editor.setDevice('Tablet');
-      }
+        editor.setDevice("Tablet");
+      },
     });
 
-    cmdm.add('set-device-mobile', {
+    cmdm.add("set-device-mobile", {
       run(editor) {
-        editor.setDevice('Mobile portrait');
-      }
+        editor.setDevice("Mobile portrait");
+      },
     });
-
   };
-})
+});
