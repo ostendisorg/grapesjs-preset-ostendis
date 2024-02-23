72de780876d3d6cd42c053cd1d4615e89367cfd5
Daniel Nüssli <daniel.nuessli@ostendis.com>
2024-02-23 19:21:49 +0100

    Remove old code

--- src/index.js
+++ src/index.js
@@ -336,12 +336,3 @@ function isChildOfElement(element, tag) {
   }
   return false;
 }
-
-function formatBytes(bytes ,decimals) {
-  if(bytes == 0) return '0 Bytes';
-  var k = 1024,
-      dm = decimals || 2,
-      sizes = ['Bytes', 'KB', 'MB', 'GB'],
-      i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-}
