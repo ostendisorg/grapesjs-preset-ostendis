e83cab6596b76e58d976d2d0b8798f9aa04113de
Daniel Nüssli <daniel.nuessli@ostendis.com>
2024-04-17 10:07:57 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
-export default ({ config }) => {
-  // This is how you can distinguish the `build` command from the `serve`
-  const isBuild = config.mode === 'production';
+const isProduction = process.env.NODE_ENV == 'production';
+const path = require('path');
 
-  return {
-      ...config,
-      entry: {
-        'grapesjs-preset-ostendis': './src/index.js',
-        'grapesjs-preset-ostendis-adv': './src/adv/index.js',
+const config = {
+  entry: {
+    'grapesjs-preset-ostendis': './src/index.ts',
+    'grapesjs-preset-ostendis-adv': './src/adv/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-      output: {
-        filename: '[name].min.js',
-        libraryTarget: 'umd',
-      },
-  };
-}
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "."),
+      watch: true,
+    },
+  },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
