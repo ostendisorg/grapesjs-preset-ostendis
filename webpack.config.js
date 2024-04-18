67b49c58f19e40e5003e72c7455d84edcc27a503
Daniel Nüssli <daniel.nuessli@ostendis.com>
2024-04-18 07:35:57 +0200

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,20 @@
-const isProduction = process.env.NODE_ENV == 'production';
-const path = require('path');
+const isProduction = process.env.NODE_ENV == "production";
 
-const config = {
-  entry: {
-    'grapesjs-preset-ostendis': './src/index.ts',
-    'grapesjs-preset-ostendis-adv': './src/adv/index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "."),
-      watch: true,
+module.exports = ({ config }) => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return {
+    ...config,
+    entry: {
+      "grapesjs-preset-ostendis": "./src/index.ts",
+      // "grapesjs-preset-ostendis-adv": "./src/adv/index.ts",
     },
-  },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+    output: {
+      ...config.output,
+      filename: "[name].min.js",
+    },
+  };
 };
